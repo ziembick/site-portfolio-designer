15478e79b5b4ba8b696e4e6e3dac247a566dd1bc
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-16 06:31:11 -0300

    icon toggler

--- portfolio-designers/src/components/navbar.jsx
+++ portfolio-designers/src/components/navbar.jsx
@@ -1,52 +1,52 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "../navbar.css";
 
 function Navbar() {
   const [active, setActive] = useState("nav__menu");
-  const [toggleIcon, setToggleIcon] = useState("nav__toggler")
-
+  const [icon, setIcon] = useState("nav__toggler");
   const navToggle = () => {
-    active === "nav__menu"
-      ? setActive("nav__menu nav__active")
-      : setActive("nav_menu");
-  };
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
 
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    <div>
-      <nav className="nav">
-        <a href="http://www.google.com" className="nav__brand">
-          Silvio Lima
-        </a>
-        <ul className={active}>
-          <li className="nav__item">
-            <a href="http://www.google.com" className="nav__link">
-              Portfolio
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="http://www.google.com" className="nav__link">
-              Sobre
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="http://www.google.com" className="nav__link">
-              Serviços
-            </a>
-          </li>
-          <li className="nav__item">
-            <a href="http://www.google.com" className="nav__link">
-              Contato
-            </a>
-          </li>
-        </ul>
-        <div onClick={navToggle} className={toggleIcon}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-      </nav>
-    </div>
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        Silvio Lima
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Sobre
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Serviços
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contato
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
   );
 }
 
