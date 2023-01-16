bc1e1753697a26ad86c6042b7b2657aadc95a7b8
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-16 05:38:29 -0300

    lines

--- portfolio-designers/src/components/navbar.jsx
+++ portfolio-designers/src/components/navbar.jsx
@@ -9,12 +9,25 @@ function Navbar() {
         </a>
         <ul className="nav__menu">
           <li className="nav__item">
-            <a href="http://www.google.com" className="nav__link">Portfolio</a>
-            <a href="http://www.google.com" className="nav__link">Sobre</a>
-            <a href="http://www.google.com" className="nav__link">Serviços</a>
-            <a href="http://www.google.com" className="nav__link">Contato</a>
+            <a href="http://www.google.com" className="nav__link">
+              Portfolio
+            </a>
+            <a href="http://www.google.com" className="nav__link">
+              Sobre
+            </a>
+            <a href="http://www.google.com" className="nav__link">
+              Serviços
+            </a>
+            <a href="http://www.google.com" className="nav__link">
+              Contato
+            </a>
           </li>
         </ul>
+        <div className="nav__toggler">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
       </nav>
     </div>
   );
