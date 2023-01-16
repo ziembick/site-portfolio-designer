5addf805935c6ea64af86a9dc8823d2298739367
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-16 07:31:52 -0300

    header css

--- portfolio-designers/src/components/header.js
+++ portfolio-designers/src/components/header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../header.css";
 
 function Header() {
   return (
-    <section>
+    <section className="section__container">
       <h1>
         I’m a graphic designer based in Porto Alegre who loves to create
         holistic brand experiences. Feel free to contact me: hi@biajanoni.com
