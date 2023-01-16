05b960c98c994c7e59d31cd8860ad8bda9001c2e
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-16 07:05:22 -0300

    footer component

--- portfolio-designers/src/App.js
+++ portfolio-designers/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function App () {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   )
 }
