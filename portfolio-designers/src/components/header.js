215b9cb33f47b94843555f2b59cc6887433cd8e1
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-19 20:27:40 -0300

    header container instagram logo

--- portfolio-designers/src/components/header.js
+++ portfolio-designers/src/components/header.js
@@ -1,12 +1,19 @@
 import React from "react";
+import igLogo from "../img/ig.png";
 import "../header.css";
 
 function Header() {
   return (
     <section className="section__container">
       <h1 className="section__container-text">
-      Designer gráfico com mais de 10 anos de experiência<br></br> trabalhando com desenvolvimento de produtos físicos e digitais.
+        Designer gráfico com mais de 10 anos de experiência<br></br> trabalhando
+        com desenvolvimento de produtos físicos e digitais.
       </h1>
+      <li>
+        <a href="http://www.instagram.com" className="section__container-ig_logo">
+          <img src={igLogo} alt="Instagram Logo" className="" />
+        </a>
+      </li>
     </section>
   );
 }
