949a3aec2f73365f06b6001e0dfe069734cb79aa
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-19 20:04:10 -0300

    header text container position

--- portfolio-designers/src/components/header.js
+++ portfolio-designers/src/components/header.js
@@ -4,7 +4,7 @@ import "../header.css";
 function Header() {
   return (
     <section className="section__container">
-      <h1>
+      <h1 className="section__container-text">
       Designer gráfico com mais de 10 anos de experiência<br></br> trabalhando com desenvolvimento de produtos físicos e digitais.
       </h1>
     </section>
