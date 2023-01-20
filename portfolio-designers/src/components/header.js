8f5879d5170499b71fafe0548c95665bb973a665
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-20 18:48:52 -0300

    dribble logo and positioning

--- portfolio-designers/src/components/header.js
+++ portfolio-designers/src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import igLogo from "../img/ig.png";
 import behance from "../img/behance.png"
+import dribble from "../img/dribble.png"
 import "../header.css";
 
 function Header() {
@@ -17,8 +18,11 @@ function Header() {
         <a href="http://www.instagram.com/slviol" className="section__container-ig">
           <img src={igLogo} alt="Instagram Logo" className="section__container-ig_logo" />
         </a>
-        <a href="http://www.behance.com/slviol" className="section__container-behance">
+        <a href="https://www.behance.net/SilvioLima" className="section__container-behance">
         <img src={behance} alt="Behance Logo" className="section__container-ig_behance_logo" />
+        </a>
+        <a href="https://www.behance.net/SilvioLima" className="section__container-dribble">
+        <img src={dribble} alt="Dribble Logo" className="section__container-ig_dribble_logo" />
         </a>
       </li>
     </section>
