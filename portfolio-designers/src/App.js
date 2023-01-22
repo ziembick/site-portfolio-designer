3b6e94f39a01270d4a7123d3629820f1f0d562c1
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-21 21:21:42 -0300

    contato page routing

--- portfolio-designers/src/App.js
+++ portfolio-designers/src/App.js
@@ -1,15 +1,67 @@
 import React from "react";
-import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import { useState } from "react";
+import Logo from "./img/Logo.png";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import Contato from "./components/contato";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+
   return (
-      <div>
-        <Navbar />
-        <Header />
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<div>
+          <nav className="nav">
+            <a href="http://www.google.com" className="nav__brand">
+              <img src={Logo} alt="Silvio Logo" className="nav__logo" />
+            </a>
+            <ul className={active}>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Sobre
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Serviços
+                </a>
+              </li>
+              <li className="nav__item">
+                <Link to="/contato" className="nav__link">
+                  Contato
+                </Link>
+              </li>
+            </ul>
+            <div onClick={navToggle} className={icon}>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+          </nav>
+          <Header />
+          <Footer />
+        </div>} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
   );
 }
 
