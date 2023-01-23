e60275eaff8667cd813732d0d5b523a45a0cc6d9
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-23 15:36:14 -0300

    navbar update and projects component

--- portfolio-designers/src/components/navbar.jsx
+++ portfolio-designers/src/components/navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Logo from "../img/Logo.png"
+import { Link, Route, Routes } from "react-router-dom";
 import "../navbar.css";
 
-function App() {
+function Navbar() {
   const [active, setActive] = useState("nav__menu");
   const [icon, setIcon] = useState("nav__toggler");
   const navToggle = () => {
@@ -16,39 +17,43 @@ function App() {
     } else setIcon("nav__toggler");
   };
   return (
-    <nav className="nav">
-      <a href="http://www.google.com" className="nav__brand">
-      <img src={Logo} alt="Silvio Logo" className="nav__logo"/>
-      </a>
-      <ul className={active}>
-        <li className="nav__item">
-          <a href="http://www.google.com" className="nav__link">
-            Portfolio
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="http://www.google.com" className="nav__link">
-            Sobre
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="http://www.google.com" className="nav__link">
-            Serviços
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="http://www.google.com" className="nav__link">
-            Contato
-          </a>
-        </li>
-      </ul>
-      <div onClick={navToggle} className={icon}>
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
+    <Routes>
+        <Route path="/" element={<div>
+          <nav className="nav">
+            <a href="http://www.google.com" className="nav__brand">
+              <img src={Logo} alt="Silvio Logo" className="nav__logo" />
+            </a>
+            <ul className={active}>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Sobre
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="http://www.google.com" className="nav__link">
+                  Serviços
+                </a>
+              </li>
+              <li className="nav__item">
+                <Link to="/contato" className="nav__link">
+                  Contato
+                </Link>
+              </li>
+            </ul>
+            <div onClick={navToggle} className={icon}>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+          </nav>
+          </div>} />
+          </Routes>
   )
 }
 
-export default App;
+export default Navbar;
