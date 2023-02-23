e460e60eaa3f12e07f83d52ceb3a97242e8c97cc
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-23 14:34:23 -0300

    imported images and contact page

--- portfolio-designers/src/components/contato.js
+++ portfolio-designers/src/components/contato.js
@@ -1,11 +1,39 @@
 import Navbar from "./navbar";
+import React, { useRef } from "react";
 
 function Contato() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>
-      <Navbar />
-      <h1>Teste teste teste teste</h1>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 }
 
