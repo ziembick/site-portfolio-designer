a8b26d717dc7610c548304497a5b647f70f5d341
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-24 16:49:41 -0300

    ponto bank page

--- portfolio-designers/src/components/ponto.js
+++ portfolio-designers/src/components/ponto.js
@@ -1,11 +1,18 @@
 import React from "react";
+import ponto1 from "../img/ponto-bank-1.gif";
+import ponto2 from "../img/ponto-bank-2.png";
+import Navbar from "./navbar";
+import "../css/ponto.css"
 
 
 function PontoBank () {
     return (
+        <>
+        <Navbar />
         <div>
-            <h1>PONTOBANK</h1>
+            <img src={ponto1} alt="Ponto Bank" className="ponto-img-1"/>
         </div>
+        </>
     )
 }
 
