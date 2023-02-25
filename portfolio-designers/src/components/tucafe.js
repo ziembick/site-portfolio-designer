97d45c74fca6c8743c2b64dbf0f5a732ee743ea8
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-25 16:26:33 -0300

    tuca cafe

--- portfolio-designers/src/components/tucafe.js
+++ portfolio-designers/src/components/tucafe.js
@@ -1,43 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./navbar";
+import "../css/ponto.css";
 
-function Tucafe() {
-  const [images, setImages] = useState();
+function PontoBank() {
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    async function loadImages(){
-      const images =[]
+    async function loadImages() {
+      const images = [];
       const files = [
-        {name: "tucacafe1.png", alt: "Tuca Cafe"},
-        {name: "tuca-cafe-2.gif", alt: "Tuca Cafe 2"},
-        {name: "tuca-cafe-3.gif", alt: "Tuca Cafe 3"},
-        {name: "tuca-cafe-4.gif", alt: "Tuca Cafe 4"},
-        {name: "tuca-cafe-5.gif", alt: "Tuca Cafe 5"},
-        {name: "tuca-cafe-6.gif", alt: "Tuca Cafe 6"},
-        {name: "tuca-cafe-7.gif", alt: "Tuca Cafe 7"},
-        {name: "tuca-cafe-8.gif", alt: "Tuca Cafe 8"},
-      ]
-      for(const file of files) {
-        const module = await import (`../img/${file.name}`)
-        images.push({...file, src: module.default})
+        { name: "tucacafe1.png", alt: "Tuca Cafe 1", className: "ponto-img-1"},
+        { name: "tuca-cafe-2.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" },
+        { name: "tuca-cafe-3.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" },
+        { name: "tuca-cafe-4.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" },
+        { name: "tuca-cafe-6.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" },
+        { name: "tuca-cafe-7.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" },
+        { name: "tuca-cafe-8.gif", alt: "Tuca Cafe 2", className: "ponto-bank-2" }
+      ];
+      for (const file of files) {
+        const module = await import(`../img/${file.name}`);
+        images.push({ ...file, src: module.default });
       }
-      setImages(images)
+      setImages(images);
     }
-    loadImages()
-  },[])
-
-
+    loadImages();
+  }, []);
 
   return (
     <>
-    <Navbar />
-    <div>
-    {images.map((image, index) => (
-      <img key={index} src={image.src} alt={image.alt} className={image.className}/>
-    ))}
-    </div>
+      <Navbar />
+      {images.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} className={image.className} />
+      ))}
     </>
   );
 }
 
-export default Tucafe;
+export default PontoBank;
