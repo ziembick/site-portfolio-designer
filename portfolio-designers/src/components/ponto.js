b1e28b66eb909c4168a9839d1ab0d4a6e415c404
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-25 16:02:53 -0300

    ponto portfolio

--- portfolio-designers/src/components/ponto.js
+++ portfolio-designers/src/components/ponto.js
@@ -1,49 +1,38 @@
-import React from "react";
-import ponto1 from "../img/ponto-bank-1.gif";
+import React, { useState, useEffect } from "react";
+import ponto1 from "../img/ponto-bank-1.gif"
 import Navbar from "./navbar";
 import "../css/ponto.css";
 
 function PontoBank() {
-  const images = [
-    { name: "ponto-bank-3.png", alt: "Ponto Bank 3" },
-    {
-      name: "ponto-bank-2.png",
-      alt: "Ponto Bank 2",
-      className: "ponto-bank-2",
-    },
-    {
-      name: "ponto-bank-4.png",
-      alt: "Ponto Bank 4",
-      className: "ponto-bank-2",
-    },
-    { name: "ponto-bank-5.png", alt: "Ponto Bank 5" },
-    {
-      name: "ponto-bank-6.png",
-      alt: "Ponto Bank 6",
-      className: "ponto-bank-2",
-    },
-    {
-      name: "ponto-bank-7.png",
-      alt: "Ponto Bank 7",
-      className: "ponto-bank-2",
-    },
-    {
-      name: "ponto-bank-8.png",
-      alt: "Ponto Bank 8",
-      className: "ponto-bank-2",
-    },
-    {
-      name: "ponto-bank-9.png",
-      alt: "Ponto Bank 9",
-      className: "ponto-bank-2",
-    },
-  ];
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function loadImages() {
+      const images = [];
+      const files = [
+        { name: "ponto-bank-3.png", alt: "Ponto Bank 3" },
+        { name: "ponto-bank-2.png", alt: "Ponto Bank 2", className: "ponto-bank-2" },
+        { name: "ponto-bank-4.png", alt: "Ponto Bank 4", className: "ponto-bank-2" },
+        { name: "ponto-bank-5.png", alt: "Ponto Bank 5" },
+        { name: "ponto-bank-6.png", alt: "Ponto Bank 6", className: "ponto-bank-2" },
+        { name: "ponto-bank-7.png", alt: "Ponto Bank 7", className: "ponto-bank-2" },
+        { name: "ponto-bank-8.png", alt: "Ponto Bank 8", className: "ponto-bank-2" },
+        { name: "ponto-bank-9.png", alt: "Ponto Bank 9", className: "ponto-bank-2" }
+      ];
+      for (const file of files) {
+        const module = await import(`../img/${file.name}`);
+        images.push({ ...file, src: module.default });
+      }
+      setImages(images);
+    }
+    loadImages();
+  }, []);
 
   return (
     <>
       <Navbar />
-      <img src={ponto1} alt="Ponto Bank" className="ponto-img-1" />
       <div>
+        <img src={ponto1} alt="Ponto Bank 1" className="ponto-img-1"/>
         <p className="ponto-p">
           Fui a fundo na criação do símbolo de ponto que da o nome a empresa,
           descobrindo que o mesmo foi criado no século III A.C e introduzida por
@@ -64,13 +53,8 @@ function PontoBank() {
           observar algumas referências coloquei a mão na massa.
         </p>
       </div>
-      {images.map((img) =>(
-        <img 
-        key={img.name}
-        src={require(`../img/${img.name}`).default}
-        alt={img.alt}
-        className={img.className}
-        />
+      {images.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} className={image.className} />
       ))}
     </>
   );
