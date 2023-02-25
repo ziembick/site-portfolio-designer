11d87faa55749aa57cc0ebabbd5644d46a09f136
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-25 16:13:55 -0300

    images tucafe

--- portfolio-designers/src/components/tucafe.js
+++ portfolio-designers/src/components/tucafe.js
@@ -6,7 +6,17 @@ function Tucafe() {
 
   useEffect(() => {
     async function loadImages(){
-      
+      const images =[]
+      const files = [
+        {name: "tucacafe1.png", alt: "Tuca Cafe"},
+        {name: "tuca-cafe-2.gif", alt: "Tuca Cafe 2"},
+        {name: "tuca-cafe-3.gif", alt: "Tuca Cafe 3"},
+        {name: "tuca-cafe-4.gif", alt: "Tuca Cafe 4"},
+        {name: "tuca-cafe-5.gif", alt: "Tuca Cafe 5"},
+        {name: "tuca-cafe-6.gif", alt: "Tuca Cafe 6"},
+        {name: "tuca-cafe-7.gif", alt: "Tuca Cafe 7"},
+        {name: "tuca-cafe-8.gif", alt: "Tuca Cafe 8"},
+      ]
     }
   },[])
 
