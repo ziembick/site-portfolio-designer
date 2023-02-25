fe00ae82598741fa2fe171afd162c87784405aa0
Paulo Ziembick <pauloziembick@gmail.com>
2023-02-25 16:08:23 -0300

    tucafe portfolio

--- portfolio-designers/src/components/tucafe.js
+++ portfolio-designers/src/components/tucafe.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "./navbar";
 
 function Tucafe() {
+  const [images, setImages] = useEffect();
+
+
+
   return (
-    <div>
-      <h1>TUCAFE</h1>
-    </div>
+    <Navbar />
   );
 }
 
