1f43c66b6263385176f593d059aa9f75e017bddf
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-01 18:54:15 -0300

    header

--- portfolio-designers/src/App.js
+++ portfolio-designers/src/App.js
@@ -33,3 +33,67 @@ function App() {
 }
 
 export default App;
+
+
+// function App() {
+//   const [active, setActive] = useState("nav__menu");
+//   const [icon, setIcon] = useState("nav__toggler");
+//   const navToggle = () => {
+//     if (active === "nav__menu") {
+//       setActive("nav__menu nav__active");
+//     } else setActive("nav__menu");
+
+//     // Icon Toggler
+//     if (icon === "nav__toggler") {
+//       setIcon("nav__toggler toggle");
+//     } else setIcon("nav__toggler");
+//   };
+
+//   return (
+//     <Routes>
+//       <Route
+//         path="/"
+//         element={
+//           <div>
+//             <nav className="nav">
+//               {/* <Link to="/" className="nav__brand">
+//                 <img src={Logo} alt="Silvio Logo" className="nav__logo" />
+//               </Link> */}
+//               <ul className={active}>
+//                 <li className="nav__item">
+//                   <Link to="/portfolio" className="nav__link">
+//                     Portfolio
+//                   </Link>
+//                 </li>
+//                 <li className="nav__item">
+//                   <a href="http://www.google.com" className="nav__link">
+//                     Sobre
+//                   </a>
+//                 </li>
+//                 <li className="nav__item">
+//                   <Link to="/contato" className="nav__link">
+//                     Contato
+//                   </Link>
+//                 </li>
+//               </ul>
+//               <div onClick={navToggle} className={icon}>
+//                 <div className="line1"></div>
+//                 <div className="line2"></div>
+//                 <div className="line3"></div>
+//               </div>
+//             </nav>
+//             <Header />
+//             <Footer />
+//           </div>
+//         }
+//       />
+//       <Route path="/portfolio/*" element={<Porftolio />} />
+//       <Route path="/contato/*" element={<Contato />} />
+//       <Route path="/header" element={<Header />} />
+//       <Route path="/ponto-bank" element={<PontoBank />} />
+//       <Route path="/tuca-cafe" element={<Tucafe />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
