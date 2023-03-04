1ab2db79a8bdd766ee76d1a4e02b45d158f23e7c
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-04 17:57:22 -0300

    styling

--- portfolio-designers/src/components/ImageSlider.js
+++ portfolio-designers/src/components/ImageSlider.js
@@ -9,18 +9,6 @@ const ImageSlider = ({ slides }) => {
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
 
-  const dotsContainerStyle = {
-    display: "flex",
-    justifyContent: "center",
-  };
-
-  const dotStyles = {
-    margin: "0 3px",
-    cursor: "pointer",
-    fontSize: "20px",
-    color: "white",
-  };
-
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
@@ -46,11 +34,11 @@ const ImageSlider = ({ slides }) => {
         ❱
       </div>
       <div className="slideStyles" style={slideStyles}></div>
-      <div style={dotsContainerStyle}>
+      <div className="dotsContainerStyle">
         {slides.map((slides, slideIndex) => (
           <div
             key={slideIndex}
-            style={dotStyles}
+            className="dotStyles"
             onClick={() => goToSlide(slideIndex)}
           >
             ●
