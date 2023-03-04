be1208a4065cd0e9584b5b35cfd69fda9fcfe049
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-04 17:04:12 -0300

    imageSlider component

--- portfolio-designers/src/components/ImageSlider.js
+++ portfolio-designers/src/components/ImageSlider.js
@@ -1,6 +1,94 @@
 import React from "react";
 import { useState } from "react";
 
-const ImageSlider = ({slider}) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-}
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const sliderStyles = {
+    height: "100%",
+    position: "relative",
+  };
+
+  const slideStyles = {
+    width: "100%",
+    height: "100%",
+    borderRadius: "10px",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
+  const leftArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    left: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const rightArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    right: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const dotsContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+  };
+
+  const dotStyles = {
+    margin: "0 3px",
+    cursor: "pointer",
+    fontSize: "20px",
+  };
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = slideIndex => {
+    setCurrentIndex(slideIndex)
+  }
+
+  return (
+    <div style={sliderStyles}>
+      <div style={leftArrowStyles} onClick={goToPrevious}>
+        ❰
+      </div>
+      <div style={rightArrowStyles} onClick={goToNext}>
+        ❱
+      </div>
+      <div style={slideStyles}></div>
+      <div style={dotsContainerStyle}>
+        {slides.map((slides, slideIndex) => (
+          <div
+            key={slideIndex}
+            style={dotStyles}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            ●
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
