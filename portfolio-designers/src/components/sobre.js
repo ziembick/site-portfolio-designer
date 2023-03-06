778f2aea1825c1a11f7ffbc005523fd371a59758
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-06 20:19:02 -0300

    about page

--- portfolio-designers/src/components/sobre.js
+++ portfolio-designers/src/components/sobre.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Navbar from "./navbar";
 import "../css/sobre.css";
+import Perfil from "../img/perfil.svg"
 
 function Sobre() {
   return (
     <>
       <section className="section__container">
         <Navbar />
+        <div className="title__container">
           <h1 className="title">Silvio Lima</h1>
-          <h2 className="title__container">
+          <h2 className="title__description">
             Designer gráfico 28 anos com mais de 10 anos de experiência,
+            <br />
             atualmente moro em São Paulo - Capital e trabalho com
-            desenvolvimento de produtos físicos e digitais. Domínio em diversos
-            programas como: Photoshop, After Effects, Figma, Illustrator,
-            CorelDraw entre outros. Transito também na área de Ux design.
+            <br />
+            desenvolvimento de produtos físicos e digitais. Domínio em <br />
+            diversos programas como: Photoshop, After Effects, Figma,
+            <br /> Illustrator, CorelDraw entre outros. Transito também na área
+            de Ux design.
           </h2>
+        </div>
+        <img src={Perfil} alt="Foto de Perfil" className="foto-perfil"/>
       </section>
     </>
   );
