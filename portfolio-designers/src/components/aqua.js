96e2f09907eea3a6b1934aacbfe3a8889224ce68
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-08 14:42:57 -0300

    aqua page

--- portfolio-designers/src/components/aqua.js
+++ portfolio-designers/src/components/aqua.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import aqua1 from "../img/aqua1.png";
 import Navbar from "./navbar";
+import "../css/ponto.css";
 
-function Aqua () {
-    return(
-        <div>
-            <h1>OI</h1>
-        </div>
-    )
+function Aqua() {
+  const [images, setImages] = useState([])
+
+
+useEffect(() => {
+  async function loadImages () {
+    const images = []
+    const files = [
+      { name: "aqua2.png", alt:"Acqua Lounge"},
+      { name: "aqua3.png", alt:"Acqua Lounge"},
+      { name: "aqua4.png", alt:"Acqua Lounge"},
+      { name: "aqua5.png", alt:"Acqua Lounge"},
+      { name: "aqua6.png", alt:"Acqua Lounge"},
+
+    ]
+    for (const file of files) {
+      const module = await import (`../img/${file.name}`)
+      images.push({...file, src: module.default})
+    }
+    setImages(images)
+  }
+  loadImages()
+},[])
+
+  return (
+    <div>
+      <h1>OI</h1>
+    </div>
+  );
 }
 
-export default Aqua
+export default Aqua;
