c5e644dfca3c3d57b1c61c9168e4555ef981ceff
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-13 18:29:21 -0300

    page not found component

---
+++ portfolio-designers/src/components/pageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function pageNotFound () {
+    return (
+        <div>
+            <h1>404 PAGE NOT FOUND</h1>
+        </div>
+    )
+}
+
+
+export default pageNotFound;
