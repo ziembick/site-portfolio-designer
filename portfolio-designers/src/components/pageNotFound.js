ad05612281a0ca1a71839cb0964e591b96c32173
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-13 18:49:25 -0300

    page not found css

--- portfolio-designers/src/components/pageNotFound.js
+++ portfolio-designers/src/components/pageNotFound.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "../css/404page.css";
+import Navbar from "./navbar";
+import smile from "../img/smile2.svg";
 
-function PageNotFound () {
-    return (
-        <div>
-            <h1>404 PAGE NOT FOUND</h1>
+function PageNotFound() {
+  return (
+    <>
+      <section className="section__container">
+        <Navbar />
+        <div className="smile-container">
+          <img src={smile} alt="Smile" className="smile-404" />
         </div>
-    )
+      </section>
+    </>
+  );
 }
-
 
 export default PageNotFound;
