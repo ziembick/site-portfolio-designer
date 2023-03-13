fc41b2c51deda363d33b47dcdc6332c7f99c7b44
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-13 18:51:23 -0300

    page not found

--- portfolio-designers/src/components/pageNotFound.js
+++ portfolio-designers/src/components/pageNotFound.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../css/404page.css";
 import Navbar from "./navbar";
 import smile from "../img/smile2.svg";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
@@ -10,6 +11,10 @@ function PageNotFound() {
         <Navbar />
         <div className="smile-container">
           <img src={smile} alt="Smile" className="smile-404" />
+          <div className="page-container">
+            <h1 className="page">Página não encontrada!</h1>
+            <Link to="/">Volte para a página principal</Link>
+          </div>
         </div>
       </section>
     </>
