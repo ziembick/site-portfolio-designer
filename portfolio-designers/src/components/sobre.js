8d042e66afa4491794c7942ce482c39910685c68
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-14 18:02:12 -0300

    spacing

--- portfolio-designers/src/components/sobre.js
+++ portfolio-designers/src/components/sobre.js
@@ -10,20 +10,34 @@ function Sobre() {
       <section className="section__container">
         <Navbar />
         <div className="title__container">
-          <Link to="/" className="arrow">←</Link>
+          <Link to="/" className="arrow">
+            ←
+          </Link>
           <h1 className="title">Silvio Lima</h1>
           <h2 className="title__description">
-            Designer gráfico 28 anos com mais de 10 anos de experiência,
+            Entendo que uma marca é muito mais do que apenas uma imagem, ela é a
+            representação visual
+            <br /> da identidade de uma empresa, sua personalidade e seus
+            valores. Como um profissional experiente <br /> em criação de
+            marcas, entendo que o processo de criação não é apenas sobre design,
+            mas também <br /> sobre entender a natureza da empresa. <br />
             <br />
-            atualmente moro em São Paulo - Capital e trabalho com
-            <br />
-            desenvolvimento de produtos físicos e digitais. Domínio em <br />
-            diversos programas como: Photoshop, After Effects, Figma,
-            <br /> Illustrator, CorelDraw entre outros. Transito também na área<br />
-            de Ux design.
+            Por isso, trabalho em estreita colaboração com meus clientes, <br />
+            a fim de mergulhar no universo da empresa e entender sua essência e
+            o que a torna especial. <br />
+            Ao compreender a sua missão, valores e visão, busco referências
+            inspiradoras e exploro diversas <br />
+            opções de formas, cores e símbolos, a fim de criar uma marca única e
+            memorável. <br /> <br /> Minha experiência em criação de marcas me
+            permite testar diferentes opções até encontrar a combinação <br />
+            perfeita que represente a empresa de forma autêntica e coerente com
+            sua identidade. Assim, garanto que <br />
+            cada marca que crio tenha um impacto positivo e duradouro no
+            público, ajudando a empresa a se destacar <br /> em um mercado
+            competitivo.
           </h2>
         </div>
-          {/* <img src={Perfil} alt="Foto de Perfil" className="foto-perfil" /> */}
+        {/* <img src={Perfil} alt="Foto de Perfil" className="foto-perfil" /> */}
       </section>
     </>
   );
