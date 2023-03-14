aa4ddfa04a01e658fc07bea37dc17ad45714b8b0
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-13 21:12:02 -0300

    hover image portfolio

--- portfolio-designers/src/components/portfolio.js
+++ portfolio-designers/src/components/portfolio.js
@@ -11,78 +11,99 @@ import "../css/portfolio.css";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import { Link } from "react-router-dom";
-import sunergy1 from "../img/sunergy.png";
-import sunergy2 from "../img/sunergy2.png";
 
-function Porftolio() {
-  const [hoveringTucafe, setHoveringTucafe] = useState(false);
-  const [hoveringPonto, setHoveringPonto] = useState(false);
-  const [hoveringAqua, setHoveringAqua] = useState(false);
-  const [hoveringProvu, setHoveringProvu] = useState(false);
-  const [hoveringSunergy, setHoveringSunergy] = useState(false);
+const Portfolio = () => {
+  const ImageSwitcher = ({ defaultImage, alternateImage }) => {
+    return (
+      <div className="image-switcher">
+        <img src={defaultImage} alt="Default" className="default-image" />
+        <img
+          src={alternateImage}
+          alt="Alternate"
+          className="alternate-image"
+        />
+      </div>
+    );
+  };
 
   return (
     <>
       <section className="section__container">
         <Navbar />
-        <Link to="/" className="arrow-portfolio">
-          ←
-        </Link>
+        <Link className="arrow-portfolio">←</Link>
         <section className="portfolio">
-          <Link to="/tuca-cafe">
-            <img
-              src={hoveringTucafe ? tucafe2 : tucafe1}
-              alt="Tuca Cafe"
-              className={`tuca-cafe ${hoveringTucafe ? "hover" : ""} `}
-              onMouseOver={() => setHoveringTucafe(true)}
-              onMouseOut={() => setHoveringTucafe(false)}
-            />
-            {/* <div className="tuca-cafe-text">Tuca Café</div> */}
-          </Link>
-          <Link to="/ponto-bank">
-            <img
-              src={hoveringPonto ? ponto2 : ponto1}
-              alt="Ponto Bank"
-              className={`ponto-bank-port ${hoveringPonto ? "hover" : ""}`}
-              onMouseOver={() => setHoveringPonto(true)}
-              onMouseOut={() => setHoveringPonto(false)}
-            />
-            {/* <p className="ponto-bank-text-port">Ponto Bank</p> */}
-          </Link>
-          <Link to="/aqua">
-            <img
-              src={hoveringAqua ? aqua2 : aqua1}
-              alt="Acqua Lounge"
-              className={`aqua ${hoveringAqua ? "hover" : ""}`}
-              onMouseOver={() => setHoveringAqua(true)}
-              onMouseOut={() => setHoveringAqua(false)}
-            />
-            {/* <p className="aqua-text">Acqua Lounge</p> */}
-          </Link>
-          <Link to="/provu">
-            <img
-              src={hoveringProvu ? provu2 : provu1}
-              alt="Provu"
-              className={`provu ${hoveringProvu ? "hover" : ""}`}
-              onMouseOver={() => setHoveringProvu(true)}
-              onMouseOut={() => setHoveringProvu(false)}
-            />
-            {/* <p className="provu-text">Provu</p> */}
-          </Link>
-          {/* <Link to="/provu">
-            <img
-              src={hoveringSunergy ? sunergy2 : sunergy1}
-              alt="Sunergy"
-              className={`provu ${hoveringSunergy ? "hover" : ""}`}
-              onMouseOver={() => setHoveringSunergy(true)}
-              onMouseOut={() => setHoveringSunergy(false)}
-            />
-          </Link> */}
+          <ImageSwitcher defaultImage={aqua1} alternateImage={aqua2} />
+          <ImageSwitcher defaultImage={tucafe1} alternateImage={tucafe2} />
+          <ImageSwitcher defaultImage={provu1} alternateImage={provu2} />
+          <ImageSwitcher defaultImage={ponto1} alternateImage={ponto2} />
         </section>
         <Footer />
       </section>
     </>
   );
-}
+};
 
-export default Porftolio;
+export default Portfolio;
+
+// function Porftolio() {
+//   const [hoveringTucafe, setHoveringTucafe] = useState(false);
+//   const [hoveringPonto, setHoveringPonto] = useState(false);
+//   const [hoveringAqua, setHoveringAqua] = useState(false);
+//   const [hoveringProvu, setHoveringProvu] = useState(false);
+
+//   return (
+//     <>
+//       <section className="section__container">
+//         <Navbar />
+//         <Link to="/" className="arrow-portfolio">
+//           ←
+//         </Link>
+//         <section className="portfolio">
+//           <Link to="/tuca-cafe">
+//             <img
+//               src={hoveringTucafe ? tucafe2 : tucafe1}
+//               alt="Tuca Cafe"
+//               className={`tuca-cafe ${hoveringTucafe ? "hover" : ""} `}
+//               onMouseOver={() => setHoveringTucafe(true)}
+//               onMouseOut={() => setHoveringTucafe(false)}
+//             />
+//             {/* <div className="tuca-cafe-text">Tuca Café</div> */}
+//           </Link>
+//           <Link to="/ponto-bank">
+//             <img
+//               src={hoveringPonto ? ponto2 : ponto1}
+//               alt="Ponto Bank"
+//               className={`ponto-bank-port ${hoveringPonto ? "hover" : ""}`}
+//               onMouseOver={() => setHoveringPonto(true)}
+//               onMouseOut={() => setHoveringPonto(false)}
+//             />
+//             {/* <p className="ponto-bank-text-port">Ponto Bank</p> */}
+//           </Link>
+//           <Link to="/aqua">
+//             <img
+//               src={hoveringAqua ? aqua2 : aqua1}
+//               alt="Acqua Lounge"
+//               className={`aqua ${hoveringAqua ? "hover" : ""}`}
+//               onMouseOver={() => setHoveringAqua(true)}
+//               onMouseOut={() => setHoveringAqua(false)}
+//             />
+//             {/* <p className="aqua-text">Acqua Lounge</p> */}
+//           </Link>
+//           <Link to="/provu">
+//             <img
+//               src={hoveringProvu ? provu2 : provu1}
+//               alt="Provu"
+//               className={`provu ${hoveringProvu ? "hover" : ""}`}
+//               onMouseOver={() => setHoveringProvu(true)}
+//               onMouseOut={() => setHoveringProvu(false)}
+//             />
+//             {/* <p className="provu-text">Provu</p> */}
+//           </Link>
+//         </section>
+//         <Footer />
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Porftolio;
