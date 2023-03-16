2d4c5d1f59334579be31536f8972e52a765f1891
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-16 16:35:39 -0300

    bttbtn function

--- portfolio-designers/src/components/backToTopButton.js
+++ portfolio-designers/src/components/backToTopButton.js
@@ -2,6 +2,19 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function BackToTopBtn() {
+  const [backToTopBtn, setBackToTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTopBtn(true)
+      } else {
+        setBackToTopBtn(false)
+      }
+    })
+  }, [])
+
+
   return (
     <div>
       <button></button>
