de2b7c5d8f9e6d150888e96171717587cdf7268e
Paulo Ziembick <pauloziembick@gmail.com>
2023-03-17 17:20:34 -0300

    image

--- portfolio-designers/src/components/loading.js
+++ portfolio-designers/src/components/loading.js
@@ -2,8 +2,12 @@ import React from "react";
 import "../css/loading.css";
 import loading from "../img/loader-gif.gif";
 
-function Loading () {
-
+function Loading() {
+  return (
+    <div className="loader_container">
+      <img className="loader" src={loading} alt="Loading" />
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
