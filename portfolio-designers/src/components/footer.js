77df4da46862c3eac6f143f687e0b90b63e456d4
Paulo Ziembick <pauloziembick@gmail.com>
2023-04-11 16:50:17 -0300

    responsividade ipad

--- portfolio-designers/src/components/footer.js
+++ portfolio-designers/src/components/footer.js
@@ -6,7 +6,6 @@ function Footer() {
   return (
     <>
       <img src={logoFooter} alt="Silvio Lima" className="nav__logo-footer" />
-      {/* <p className="testesteste">Silvio Lima</p> */}
     </>
   );
 }
