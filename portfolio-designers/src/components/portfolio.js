6b6003cf1a8c5216c752ac972cef300e62417049
Paulo Ziembick <pauloziembick@gmail.com>
2023-04-13 17:18:58 -0300

    port page

--- portfolio-designers/src/components/portfolio.js
+++ portfolio-designers/src/components/portfolio.js
@@ -15,6 +15,7 @@ import "../css/portfolio.css";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import { Link } from "react-router-dom";
+import { Fade } from "react-reveal";
 
 const Portfolio = () => {
   const ImageSwitcher = ({ defaultImage, alternateImage }) => {
@@ -34,24 +35,29 @@ const Portfolio = () => {
           ←
         </Link>
         <section className="portfolio">
-          <Link to="/aqua">
-            <ImageSwitcher defaultImage={aqua1} alternateImage={aqua2} />
-          </Link>
-          <Link to="/tuca-cafe">
-            <ImageSwitcher defaultImage={tucafe1} alternateImage={tucafe2} />
-          </Link>
-          <Link to="/provu">
-            <ImageSwitcher defaultImage={provu1} alternateImage={provu2} />
-          </Link>
-          <Link to="/ponto-bank">
-            <ImageSwitcher defaultImage={ponto1} alternateImage={ponto2} />
-          </Link>
-          <Link to="/ydent">
-            <ImageSwitcher defaultImage={ydent1} alternateImage={ydent2} />
-          </Link>
-          <Link to="/sunergy">
-            <ImageSwitcher defaultImage={sunergy1} alternateImage={sunergy2} />
-          </Link>
+          <Fade top distance="1%" duration={1100}>
+            <Link to="/aqua">
+              <ImageSwitcher defaultImage={aqua1} alternateImage={aqua2} />
+            </Link>
+            <Link to="/tuca-cafe">
+              <ImageSwitcher defaultImage={tucafe1} alternateImage={tucafe2} />
+            </Link>
+            <Link to="/provu">
+              <ImageSwitcher defaultImage={provu1} alternateImage={provu2} />
+            </Link>
+            <Link to="/ponto-bank">
+              <ImageSwitcher defaultImage={ponto1} alternateImage={ponto2} />
+            </Link>
+            <Link to="/ydent">
+              <ImageSwitcher defaultImage={ydent1} alternateImage={ydent2} />
+            </Link>
+            <Link to="/sunergy">
+              <ImageSwitcher
+                defaultImage={sunergy1}
+                alternateImage={sunergy2}
+              />
+            </Link>
+          </Fade>
         </section>
       </section>
       <Footer />
@@ -61,65 +67,3 @@ const Portfolio = () => {
 
 export default Portfolio;
 
-// function Porftolio() {
-//   const [hoveringTucafe, setHoveringTucafe] = useState(false);
-//   const [hoveringPonto, setHoveringPonto] = useState(false);
-//   const [hoveringAqua, setHoveringAqua] = useState(false);
-//   const [hoveringProvu, setHoveringProvu] = useState(false);
-
-//   return (
-//     <>
-//       <section className="section__container">
-//         <Navbar />
-//         <Link to="/" className="arrow-portfolio">
-//           ←
-//         </Link>
-//         <section className="portfolio">
-//           <Link to="/tuca-cafe">
-//             <img
-//               src={hoveringTucafe ? tucafe2 : tucafe1}
-//               alt="Tuca Cafe"
-//               className={`tuca-cafe ${hoveringTucafe ? "hover" : ""} `}
-//               onMouseOver={() => setHoveringTucafe(true)}
-//               onMouseOut={() => setHoveringTucafe(false)}
-//             />
-//             {/* <div className="tuca-cafe-text">Tuca Café</div> */}
-//           </Link>
-//           <Link to="/ponto-bank">
-//             <img
-//               src={hoveringPonto ? ponto2 : ponto1}
-//               alt="Ponto Bank"
-//               className={`ponto-bank-port ${hoveringPonto ? "hover" : ""}`}
-//               onMouseOver={() => setHoveringPonto(true)}
-//               onMouseOut={() => setHoveringPonto(false)}
-//             />
-//             {/* <p className="ponto-bank-text-port">Ponto Bank</p> */}
-//           </Link>
-//           <Link to="/aqua">
-//             <img
-//               src={hoveringAqua ? aqua2 : aqua1}
-//               alt="Acqua Lounge"
-//               className={`aqua ${hoveringAqua ? "hover" : ""}`}
-//               onMouseOver={() => setHoveringAqua(true)}
-//               onMouseOut={() => setHoveringAqua(false)}
-//             />
-//             {/* <p className="aqua-text">Acqua Lounge</p> */}
-//           </Link>
-//           <Link to="/provu">
-//             <img
-//               src={hoveringProvu ? provu2 : provu1}
-//               alt="Provu"
-//               className={`provu ${hoveringProvu ? "hover" : ""}`}
-//               onMouseOver={() => setHoveringProvu(true)}
-//               onMouseOut={() => setHoveringProvu(false)}
-//             />
-//             {/* <p className="provu-text">Provu</p> */}
-//           </Link>
-//         </section>
-//         <Footer />
-//       </section>
-//     </>
-//   );
-// }
-
-// export default Porftolio;
